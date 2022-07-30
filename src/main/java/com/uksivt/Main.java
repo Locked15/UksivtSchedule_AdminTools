3097110ce4acb193f7cc8b1ac5c7a49d717906f0
Алмаз <almgal353@gmail.com>
2022-07-30 13:50:39 +0500

    Updated dependencies.

--- src/main/java/com/uksivt/Main.java
+++ src/main/java/com/uksivt/Main.java
@@ -23,7 +23,7 @@ public class Main
      */
     public static void main(String[] args)
     {
-
+        System.out.println("Hello, World!");
     }
 
     //region Область: Методы добавления данных.
